896656c0345112a7cc9e96bcb9553eafd21dcd39
SongZihuan <songzihuan@song-zh.com>
2024-12-22 00:07:50 +0800

    feat: #13版本13

---
+++ src/ginhttp/data/code.go
@@ -0,0 +1,35 @@
+package data
+
+type CodeType int64
+
+const (
+	GlobalCodeOk                    CodeType = 0
+	GlobalCodeErrorUnknown          CodeType = 1
+	GlobalCodeErrorTokenExpire      CodeType = 2
+	GlobalCodeErrorNoPermission     CodeType = 3
+	GlobalCodeErrorNoRootPermission CodeType = 4
+	GlobalCodeErrorNotTestMode      CodeType = 5
+	GlobalCodeErrorDatabase         CodeType = 6
+)
+
+var CodeDebugMsg = map[CodeType]string{
+	GlobalCodeOk:                    "正常",
+	GlobalCodeErrorUnknown:          "未知错误",
+	GlobalCodeErrorTokenExpire:      "Token过期",
+	GlobalCodeErrorNoPermission:     "无访问权限（Admin）",
+	GlobalCodeErrorNoRootPermission: "无访问权限（Root Admin）",
+	GlobalCodeErrorNotTestMode:      "非测试模式",
+	GlobalCodeErrorDatabase:         "数据库错误",
+}
+
+func GetCodeDebugMsg(code CodeType) string {
+	if code < 0 {
+		return "业务错误"
+	}
+
+	debugMsg, ok := CodeDebugMsg[code]
+	if ok {
+		return debugMsg
+	}
+	return "未知错误"
+}
