870e952e54ac068507d08394e2b7cdc0544f4809
SongZihuan <songzihuan@song-zh.com>
2024-12-25 20:52:49 +0800

    feat: #23版本23

--- src/ginhttp/handler/test/secret/user/pay/testpay/data.go
+++ src/ginhttp/handler/test/secret/user/pay/testpay/data.go
@@ -1,18 +1,6 @@
 package testpay
 
-import "github.com/SuperH-0630/cat-shop-back/src/ginhttp/data"
-
 type Query struct {
-	ID uint `form:"id"`
-}
-
-type Data struct {
-}
-
-func NewData() Data {
-	return Data{}
-}
-
-func NewJsonData() data.Data {
-	return data.NewSuccessWithData(NewData())
+	ID       uint `form:"id"`
+	FailRate int  `form:"failrate"`
 }
