17d15fd027d462255eb1c7b75e6c3dc5ef919a40
SongZihuan <songzihuan@song-zh.com>
2025-01-05 19:08:09 +0800

    移动并重构配置数据处理代码
    将`rootadmin`下的配置数据处理代码移动到`admin`目录下，并对结构体和函数名称进行了更新，以更好地反映其用途。

---
+++ src/ginhttp/handler/v1/secret/admin/admingetconfig/data.go
@@ -0,0 +1,45 @@
+package admingetconfig
+
+import (
+	"github.com/SuperH-0630/cat-shop-back/src/ginhttp/data"
+	"github.com/SuperH-0630/cat-shop-back/src/model"
+	"github.com/SuperH-0630/cat-shop-back/src/model/modeltype"
+)
+
+type ConfigKeyValue map[modeltype.ConfigKeyType]modeltype.ConfigValueType
+
+type Data struct {
+	Config   ConfigKeyValue                             `json:"config"`
+	Info     map[modeltype.ConfigKeyType]string         `json:"info"`
+	Type     map[modeltype.ConfigKeyType]ConfigItemType `json:"type"`
+	TypeName map[ConfigItemType]string                  `json:"typename"`
+}
+
+func NewConfigKeyValue(cfg []model.Config) ConfigKeyValue {
+	config := make(ConfigKeyValue, len(modeltype.ConfigKey))
+	for _, k := range modeltype.ConfigKey {
+		config[k] = ""
+	}
+
+	for _, r := range cfg {
+		_, ok := config[r.Key] // 检测是否存在，用于确定r.Key是否存在于ConfigKey
+		if ok {
+			config[r.Key] = r.GetValue()
+		}
+	}
+
+	return config
+}
+
+func NewData(cfg []model.Config) Data {
+	return Data{
+		Config:   NewConfigKeyValue(cfg),
+		Info:     AdminConfigInfo,
+		Type:     AdminConfigType,
+		TypeName: AdminConfigTypeInfo,
+	}
+}
+
+func NewJsonData(cfg []model.Config) data.Data {
+	return data.NewSuccessWithData(NewData(cfg))
+}
