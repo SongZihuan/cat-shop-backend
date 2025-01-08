17d0984f6dbd90fe20f582d0f5f8ddd987060a95
宋子桓🌈 <songzihuan@song-zh.com>
2025-01-08 15:04:51 +0800

    优化Token处理逻辑
    调整了`handlerToken`函数中的Token处理顺序，首先检查Token是否存在，若不存在则直接返回`TokenStatusNotToken`。接着解析Token并处理用户状态，确保流程更加清晰和合理。

--- src/ginhttp/middleware/xtoken.go
+++ src/ginhttp/middleware/xtoken.go
@@ -23,34 +23,35 @@ const (
 
 func handlerToken(c *gin.Context) (*jwttoken.Data, *model.User, TokenStatus) {
 	token := c.GetHeader(header.RequestXTokenHeader)
-	if token != "" {
-		tokenData, err := jwttoken.ParserUserToken(token)
-		if err != nil {
-			c.Set(contextkey.DebugTokenKey, "Token解析失败: "+err.Error())
-			return nil, nil, TokenStatusExpired
-		}
 
-		user, err := action.MiddlewareGetUserByID(tokenData.Userid())
-		if errors.Is(err, gorm.ErrRecordNotFound) {
-			c.Set(contextkey.DebugTokenKey, "用户未找到")
-			return nil, nil, TokenStatusUserNotFound
-		} else if err != nil {
-			c.Set(contextkey.DebugTokenKey, "Token解析失败: "+err.Error())
-			return nil, nil, TokenStatusUserNotFound
-		} else if !user.CanLogin() {
-			c.Set(contextkey.DebugTokenKey, "用户非正常状态")
-			return nil, nil, TokenStatusUserNotOk
-		}
-
-		c.Set(contextkey.TokenKey, token)
-		c.Set(contextkey.UserIDKey, user.ID)
-		c.Set(contextkey.UserKey, user)
-		c.Set(contextkey.DebugTokenKey, "正常")
-		return &tokenData, user, TokenStatusHasUser
-	} else {
+	if token == "" {
 		c.Set(contextkey.DebugTokenKey, "没有Token")
 		return nil, nil, TokenStatusNotToken
 	}
+
+	tokenData, err := jwttoken.ParserUserToken(token)
+	if err != nil {
+		c.Set(contextkey.DebugTokenKey, "Token解析失败: "+err.Error())
+		return nil, nil, TokenStatusExpired
+	}
+
+	user, err := action.MiddlewareGetUserByID(tokenData.Userid())
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		c.Set(contextkey.DebugTokenKey, "用户未找到")
+		return nil, nil, TokenStatusUserNotFound
+	} else if err != nil {
+		c.Set(contextkey.DebugTokenKey, "Token解析失败: "+err.Error())
+		return nil, nil, TokenStatusUserNotFound
+	} else if !user.CanLogin() {
+		c.Set(contextkey.DebugTokenKey, "用户非正常状态")
+		return nil, nil, TokenStatusUserNotOk
+	}
+
+	c.Set(contextkey.TokenKey, token)
+	c.Set(contextkey.UserIDKey, user.ID)
+	c.Set(contextkey.UserKey, user)
+	c.Set(contextkey.DebugTokenKey, "正常")
+	return &tokenData, user, TokenStatusHasUser
 }
 
 func handlerResetToken(c *gin.Context, tokenData *jwttoken.Data, user *model.User, status TokenStatus) {
